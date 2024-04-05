d152f0e08f9653c4276dd7aed702e587edf3c7f8
m23csa007 <165289948+m23csa007@users.noreply.github.com>
2024-04-06 04:00:54 +0530

    Update generate_librimix.sh
    add directories to run in kaggle

--- generate_librimix.sh
+++ generate_librimix.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 set -eu  # Exit on error
 
-storage_dir=$1
-librispeech_dir=$storage_dir/LibriSpeech
-wham_dir=$storage_dir/wham_noise
-librimix_outdir=$storage_dir/
+# Define the storage directory path
+storage_dir="/kaggle/working"
 
-function LibriSpeech_test_clean() {
-    if ! test -e $librispeech_dir/test-clean; then
-        echo "Download LibriSpeech/test-clean into $storage_dir"
-        # If downloading stalls for more than 20s, relaunch from previous state.
-        wget -c --tries=0 --read-timeout=20 http://www.openslr.org/resources/12/test-clean.tar.gz -P $storage_dir
-        tar -xzf $storage_dir/test-clean.tar.gz -C $storage_dir
-        rm -rf $storage_dir/test-clean.tar.gz
-    fi
-}
+# Define the directory paths for LibriSpeech, WHAM, and LibriMix datasets
+librispeech_dir="/kaggle/input/librispeech/LibriSpeech"
+wham_dir="${storage_dir}/wham_noise"
+librimix_outdir="${storage_dir}"
 
 function wham() {
-    if ! test -e $wham_dir; then
+    if ! test -e "$wham_dir"; then
         echo "Download wham_noise into $storage_dir"
         # If downloading stalls for more than 20s, relaunch from previous state.
-        wget -c --tries=0 --read-timeout=20 https://my-bucket-a8b4b49c25c811ee9a7e8bba05fa24c7.s3.amazonaws.com/wham_noise.zip -P $storage_dir
-        unzip -qn $storage_dir/wham_noise.zip -d $storage_dir
-        rm -rf $storage_dir/wham_noise.zip
+        wget -c --tries=0 --read-timeout=20 https://my-bucket-a8b4b49c25c811ee9a7e8bba05fa24c7.s3.amazonaws.com/wham_noise.zip -P "$storage_dir"
+        unzip -qn "$storage_dir/wham_noise.zip" -d "$storage_dir"
+        rm -rf "$storage_dir/wham_noise.zip"
     fi
 }
 
-LibriSpeech_test_clean &
+# Execute functions to download datasets concurrently
 wham &
 wait
 
@@ -35,10 +28,10 @@ python_path=python
 
 # Generate LibriMix dataset for 2 speakers
 metadata_dir=metadata/Libri2Mix
-$python_path scripts/create_librimix_from_metadata.py --librispeech_dir $librispeech_dir \
-    --wham_dir $wham_dir \
-    --metadata_dir $metadata_dir \
-    --librimix_outdir $librimix_outdir \
+"$python_path" scripts/create_librimix_from_metadata.py --librispeech_dir "$librispeech_dir" \
+    --wham_dir "$wham_dir" \
+    --metadata_dir "$metadata_dir" \
+    --librimix_outdir "$librimix_outdir" \
     --n_src 2 \
     --freqs 8k 16k \
     --modes min max \
