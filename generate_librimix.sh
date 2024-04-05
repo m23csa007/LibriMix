57dca563c19feabff8616bdf558bc312d07bc469
m23csa007 <165289948+m23csa007@users.noreply.github.com>
2024-04-06 03:39:36 +0530

    Update generate_librimix.sh
    wham added

--- generate_librimix.sh
+++ generate_librimix.sh
@@ -3,33 +3,43 @@ set -eu  # Exit on error
 
 storage_dir=$1
 librispeech_dir=$storage_dir/LibriSpeech
+wham_dir=$storage_dir/wham_noise
 librimix_outdir=$storage_dir/
 
-
 function LibriSpeech_test_clean() {
-	if ! test -e $librispeech_dir/test-clean; then
-		echo "Download LibriSpeech/test-clean into $storage_dir"
-		# If downloading stalls for more than 20s, relaunch from previous state.
-		wget -c --tries=0 --read-timeout=20 http://www.openslr.org/resources/12/test-clean.tar.gz -P $storage_dir
-		tar -xzf $storage_dir/test-clean.tar.gz -C $storage_dir
-		rm -rf $storage_dir/test-clean.tar.gz
-	fi
+    if ! test -e $librispeech_dir/test-clean; then
+        echo "Download LibriSpeech/test-clean into $storage_dir"
+        # If downloading stalls for more than 20s, relaunch from previous state.
+        wget -c --tries=0 --read-timeout=20 http://www.openslr.org/resources/12/test-clean.tar.gz -P $storage_dir
+        tar -xzf $storage_dir/test-clean.tar.gz -C $storage_dir
+        rm -rf $storage_dir/test-clean.tar.gz
+    fi
 }
 
-LibriSpeech_test_clean 
+function wham() {
+    if ! test -e $wham_dir; then
+        echo "Download wham_noise into $storage_dir"
+        # If downloading stalls for more than 20s, relaunch from previous state.
+        wget -c --tries=0 --read-timeout=20 https://my-bucket-a8b4b49c25c811ee9a7e8bba05fa24c7.s3.amazonaws.com/wham_noise.zip -P $storage_dir
+        unzip -qn $storage_dir/wham_noise.zip -d $storage_dir
+        rm -rf $storage_dir/wham_noise.zip
+    fi
+}
+
+LibriSpeech_test_clean &
+wham &
 wait
 
 # Path to python
 python_path=python
 
 # Generate LibriMix dataset for 2 speakers
-for n_src in 2; do
-  metadata_dir=metadata/Libri$n_src"Mix"
-  $python_path scripts/create_librimix_from_metadata.py --librispeech_dir $librispeech_dir \
+metadata_dir=metadata/Libri2Mix
+$python_path scripts/create_librimix_from_metadata.py --librispeech_dir $librispeech_dir \
+    --wham_dir $wham_dir \
     --metadata_dir $metadata_dir \
     --librimix_outdir $librimix_outdir \
-    --n_src $n_src \
+    --n_src 2 \
     --freqs 8k 16k \
     --modes min max \
-    --types mix_clean 
-done
+    --types mix_clean mix_both mix_single
