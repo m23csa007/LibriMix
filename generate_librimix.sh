b9810a91de069b448dbf9b2e399ddfd6241fd5f7
m23csa007 <165289948+m23csa007@users.noreply.github.com>
2024-04-06 11:39:54 +0530

    Update generate_librimix.sh
    wham noise dataset file added in kaggle

--- generate_librimix.sh
+++ generate_librimix.sh
@@ -6,22 +6,8 @@ storage_dir="/kaggle/working"
 
 # Define the directory paths for LibriSpeech, WHAM, and LibriMix datasets
 librispeech_dir="/kaggle/input/librispeech/LibriSpeech"
-wham_dir="${storage_dir}/wham_noise"
+wham_dir="/kaggle/input/wham-noise/wham_noise"
 librimix_outdir="${storage_dir}"
-
-function wham() {
-    if ! test -e "$wham_dir"; then
-        echo "Download wham_noise into $storage_dir"
-        # If downloading stalls for more than 20s, relaunch from previous state.
-        wget -c --tries=0 --read-timeout=20 https://my-bucket-a8b4b49c25c811ee9a7e8bba05fa24c7.s3.amazonaws.com/wham_noise.zip -P "$storage_dir"
-        unzip -qn "$storage_dir/wham_noise.zip" -d "$storage_dir"
-        rm -rf "$storage_dir/wham_noise.zip"
-    fi
-}
-
-# Execute functions to download datasets concurrently
-wham &
-wait
 
 # Path to python
 python_path=python
